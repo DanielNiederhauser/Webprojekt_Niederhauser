611ee19fb88eb0f2299238e022915d38174098d6
daniel.niederh@gmail.com <daniel.niederh@gmail.com>
2020-10-14 16:40:27 +0200

    Initialisieren

---
+++ Webprojekt_Niederhauser/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Webprojekt_Niederhauser.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        public IActionResult Impressum()
+        {
+            return View();
+        }
+
+
+    }
+}
